e0bfae582c277aba0852be7c96e22d2b41aa6ff3
Vivek Kadam <kadamvivek5708@gmail.com>
2025-09-30 23:45:38 +0530

    user login and logout controller, middleware, routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -121,6 +121,50 @@ const loginUser = asyncHandler(async (req,res) => {
     const loggedInUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
+
+    const options ={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refreshToken",refreshToken,options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    loggedInUser,accessToken,refreshToken
+                },
+                "User Logged In Successfully"
+            )
+        )
 })
 
-export  {registerUser,loginUser}
+const logoutUser = asyncHandler(async(req,res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    const options ={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+            .status(200)
+            .clearCookie("accessToken",options)
+            .clearCookie("refreshToken",options)
+            .json(new ApiResponse(200,{}, "User Logged Out"))
+})
+
+export  {registerUser,loginUser,logoutUser}
