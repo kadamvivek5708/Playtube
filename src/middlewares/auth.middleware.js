dbdc6002e8a5a06f8e734b6228c662519f163faf
Vivek Kadam <kadamvivek5708@gmail.com>
2025-10-04 00:05:43 +0530

    add subscription model

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken"
 import { User } from "../models/users.model.js";
 
 
-export const verifyJWT = asyncHandler(async(req,_,next) => {
+export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
     
